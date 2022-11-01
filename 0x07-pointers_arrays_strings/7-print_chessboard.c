9cb18bb06a82b680e7961a7f296bccda98ce235c
GiddyLesGid <gideonlangat@gmail.com>
2022-11-01 08:41:39 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,17 +7,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
+	int x;
+	int y;
 
-	for (i = 0; i < 64; i++)
+	for (x = 0; x < 8; x++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (y = 0; y < 8; y++)
 		{
-			m = i;
 			_putchar('\n');
 		}
-		_putchar(a[i / 8][i - m]);
+	        _putchar('\n');
 	}
-	_putchar('\n');
 }
+
 
